5e53a546b9018ac761841774031ecbe6518d47fc
Steve Cirelli <scirelli@gmail.com>
2017-11-29 17:19:28 -0500

    update

--- phoneHome.sh
+++ phoneHome.sh
@@ -4,4 +4,8 @@ curl -L --head \
     --silent \
     "https://cirelli.org/operator/?id=${phoneHomeId}" > /dev/null 2>&1
 
-ssh "$phoneHomeId@cirelli.org" > /dev/null 2>&1
+if [ -v ${phoneHomePort} ]; then
+    phoneHomePort=22
+fi
+
+ssh -p $phoneHomePort "$phoneHomeId@cirelli.org" > /dev/null 2>&1
